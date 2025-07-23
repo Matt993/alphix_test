5c24dda5e44dafecbbab36189e0c5c5bb9faea1f
Matt993 <matt_993@hotmail.co.uk>
2025-07-23 15:54:38 +0100

    add date filtering

--- article_search.py
+++ article_search.py
@@ -1,24 +1,35 @@
 """File for storing and searching for relevant articles."""
 
 import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 
 class ArticleEmbeddingSearch:
     """Store articles as embeddings and search for most relevant articles to used in conjunction with client URLs for ad generation."""
-    def __init__(self, article_summaries: list[str]):
+    def __init__(self, articles_df: pd.DataFrame):
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
-        self.article_summaries = article_summaries
-        self.article_embeddings = self._store_articles(article_summaries)
+        self.articles_df = articles_df
+        self.article_summaries = articles_df['summary'].apply(lambda x: x['article_summary']).tolist()
+        self.articles_df['embedding'] = self._store_articles(self.article_summaries)
+
 
     def _store_articles(self, article_summaries: list[str]) -> list:
         """Store article summaries as embeddings."""
         article_embeddings = self.model.encode(article_summaries)
         return article_embeddings.tolist()
 
-    def search(self, query_str: str, top_k: int = 5) -> list:
-        """Search for top k most relevant articles."""
+    def search(self, query_str: str, top_k: int = 5, from_date: datetime = datetime.now() - timedelta(days=30)) -> list:
+        """Search for top k most relevant articles newer than from_date."""
+        filtered_df = self.articles_df[self.articles_df['Published date'] >= from_date]
+        if filtered_df.empty:
+            return []
         query_embed = self.model.encode([query_str])
-        similarities = cosine_similarity(query_embed, self.article_embeddings)
-        most_similar_indices = np.argsort(similarities[0])[-top_k:][::-1]
-        return [self.article_summaries[i] for i in most_similar_indices]
+        embeddings = np.vstack(filtered_df['embedding'].values)
+        similarities = cosine_similarity(query_embed, embeddings)[0]
+        # Get top-k indices
+        top_indices = np.argsort(similarities)[-top_k:][::-1]
+        # Return the corresponding summaries
+        top_summaries = filtered_df.iloc[top_indices]['summary'].apply(lambda x: x['article_summary']).tolist()
+        return [summary for summary in top_summaries if summary]  # Remove any empty summaries if filter led to them being kept. 
